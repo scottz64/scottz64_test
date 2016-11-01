e8ec35f6a3544a5b3ee524b8f23c81535926eb2f
Scott Zwierzynski <scottz@us.ibm.com>
2016-11-01 18:07:10 -0400

    draft

---
+++ bddtests_proposed/regression/daily_test_suite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#                GO SDK - Consensus CAT*                 (uses REST) scott
+#                GO SDK - Ledger LST*                    (uses REST) scott
+#                GO SDK - Concurrency/Ledger             (uses REST) scott/nishi
+#                Node.js Concurrency with Auction        (uses Node) nishi
+#                Complex Transactions with Auction       (uses Node) nishi
+#                Performance engine Baseline tests       (uses Node) dongming
+
---
+++ bddtests_proposed/regression/longrun_test_suite.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#                GO SDK - Consensus CRT*, LargeNetwks    (uses REST) scott
+#                GO SDK - Ledger Long LST*               (uses REST) scott/nishi
+#                Node.js Concurrency with Auction        (uses Node) nishi
+#                Complex Transactions with Auction       (uses Node) nishi
+#                Performance engine tests                (uses Node) dongming
+#                 - verify can handle fixed-rate "supported" number of Tx per sec
+#                   for various durations, 1 or 10 or 60 minutes
+#                 - measure for variable traffic rates, and burstiness
+#                 - find the max Tx per sec that can be handled in 1 or 10 or 60 minutes
+#                 - sequential and concurrency
+
