926bb6fdf06c58154c55ac89db74d9793e3fee08
Scott Zwierzynski <scottz@us.ibm.com>
2016-11-01 18:42:16 -0400

    test suites tests draft

--- bddtests_proposed/regression/daily_test_suite.sh
+++ bddtests_proposed/regression/daily_test_suite.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
-#                GO SDK - Consensus CAT*                 (uses REST) scott
-#                GO SDK - Ledger LST*                    (uses REST) scott
-#                GO SDK - Concurrency/Ledger             (uses REST) scott/nishi
-#                Node.js Concurrency with Auction        (uses Node) nishi
-#                Complex Transactions with Auction       (uses Node) nishi
-#                Performance engine Baseline tests       (uses Node) dongming
+#   GO SDK - Consensus CAT*                         (uses REST) scott
+#   GO SDK - Ledger LST*                            (uses REST) scott
+#   GO SDK - Concurrency/Ledger                     (uses REST) scott/nishi
+#   Node.js Concurrency with Auction                (uses gRPC) nishi
+#   Node.js Complex Transactions with Auction       (uses gRPC) nishi
+#   Node.js Performance engine Baseline tests       (uses gRPC) dongming
+
+
+# UNDER CONSTRUCTION...
+
+
+DAILY
+
+release-criteria/fvt/consensus/obcsdk/automation/run_all_CAT_tests.sh
+- CAT/CAT_*
+release-criteria/fvt/consensus/obcsdk/automation/run_LST_regression.sh
+- BasicFuncExistingNetworkLST.go
+- LST_1client1peer20K.go
+- LST_2client1peer20K.go
+- LST_2client2peer20K.go
+- LST_4client1peer20K.go
+- LST_4client4peer20K.go
+release-criteria/svt/concurrency/run_conc4p1min1000Thrd1TxPerLoop_LOCAL.sh
+- conc4p1min1000Thrd1TxPerLoop.go
+release-criteria/svt/concurrency/run_conc4p1min400Thrd_LOCAL.sh
+- conc4p1min400Thrd.go
+Run Concurrency with Auction (test missing)
+run_complexTx_auction.sh
+- 
+performancetestsDaily/...
+- 
+
+
+LONGRUN
+
+release-criteria/fvt/consensus/obcsdk/automation/run_largeNetworksBasicApiAndConsensus.sh
+- CAT/CRT_50504_Npeers_Sf_S_R_Rf.go
+release-criteria/fvt/consensus/obcsdk/automation/run_long_CRT.sh , includes:
+-  obcsdk/CAT/CRT_501_StopAndRestartRandom_10Hrs.go	# note: test must be modified to actually specify timelength
+-  obcsdk/CAT/CRT_502_StopAndRestart1or2_10Hrs.go	# note: test must be modified to actually specify timelength
+release-criteria/fvt/consensus/obcsdk/automation/run_LST_3M.sh
+- LST_4client1peer1M.go and/or
+- LST_4client4peer3M.go and/or combine with next one:
+release-criteria/svt/longruns/run_long_run_72hour_LOCAL.sh
+- LongRun72hrAuto.go
+performancetestsLongrun/...
+- 
 
--- bddtests_proposed/regression/longrun_test_suite.sh
+++ bddtests_proposed/regression/longrun_test_suite.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-#                GO SDK - Consensus CRT*, LargeNetwks    (uses REST) scott
-#                GO SDK - Ledger Long LST*               (uses REST) scott/nishi
-#                Node.js Concurrency with Auction        (uses Node) nishi
-#                Complex Transactions with Auction       (uses Node) nishi
-#                Performance engine tests                (uses Node) dongming
-#                 - verify can handle fixed-rate "supported" number of Tx per sec
-#                   for various durations, 1 or 10 or 60 minutes
-#                 - measure for variable traffic rates, and burstiness
-#                 - find the max Tx per sec that can be handled in 1 or 10 or 60 minutes
-#                 - sequential and concurrency
+#   GO SDK - Consensus CRT*, LargeNetwks                 (uses REST) scott
+#   GO SDK - Ledger Long LST*                            (uses REST) scott/nishi
+#   GO SDK - Concurrency/Ledger Long LST*                (uses REST) scott/nishi
+#   Node.js Concurrency with Auction                     (uses gRPC) nishi
+#   Node.js Complex Transactions with Auction            (uses gRPC) nishi
+#   Node.js Performance engine tests                     (uses gRPC) dongming
+#   - verify can handle fixed-rate "supported" number of Tx per sec
+#     for various durations, 1 or 10 or 60 minutes
+#   - measure for variable traffic rates, and burstiness
+#   - find the max Tx per sec that can be handled in 1 or 10 or 60 minutes
+#   - sequential and concurrency
+
+
+# UNDER CONSTRUCTION...
+
+
+LONGRUN
+
+release-criteria/fvt/consensus/obcsdk/automation/run_largeNetworksBasicApiAndConsensus.sh
+- CAT/CRT_50504_Npeers_Sf_S_R_Rf.go
+release-criteria/fvt/consensus/obcsdk/automation/run_long_CRT.sh , includes:
+-  obcsdk/CAT/CRT_501_StopAndRestartRandom_10Hrs.go	# note: test must be modified to actually specify timelength
+-  obcsdk/CAT/CRT_502_StopAndRestart1or2_10Hrs.go	# note: test must be modified to actually specify timelength
+release-criteria/fvt/consensus/obcsdk/automation/run_LST_3M.sh
+- LST_4client1peer1M.go and/or
+- LST_4client4peer3M.go and/or combine with next one:
+release-criteria/svt/longruns/run_long_run_72hour_LOCAL.sh
+- LongRun72hrAuto.go
+Node.js Concurrency with Auction (Test missing!)
+performancetestsLongrun/...
+- 
 
